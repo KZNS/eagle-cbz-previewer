b19f4f034c72cc67d475f5f23644c31b17275f4a
KZNS <kzns@foxmail.com>
2025-02-02 15:16:48 +0800

    feat: get first img as cover

--- js/cbz-util.js
+++ js/cbz-util.js
@@ -1,14 +1,32 @@
 const fs = require('fs');
 const jszip = require('jszip');
+const path = require('path');
+const sharp = require('sharp');
+
+const IMAGE_EXTENSIONS = ['.png', '.jpg', '.jpeg', '.gif', '.webp'];
+
+// 判断文件是否是图片
+function isImageFile(filePath) {
+    const ext = path.extname(filePath).toLowerCase();
+    return IMAGE_EXTENSIONS.includes(ext);
+}
 
 async function cbzCover(src, dist) {
     return new Promise(async (resolve, reject) => {
         try {
             let buffer = await fs.promises.readFile(src);
             const zip = await jszip.loadAsync(buffer);
-            const cover = zip.files['test/cover.png'];
+            let cover = null;
+            for (const relativePath of Object.keys(zip.files)) {
+                const file = zip.files[relativePath];
+                if (!file.dir && isImageFile(relativePath)) {
+                    cover = file;
+                    break;
+                }
+            }
             if (cover) {
-                let data = await cover.async('nodebuffer');
+                const data = await cover.async('nodebuffer');
+                //await sharp(data).toFormat('png').toFile(dist);
                 await fs.promises.writeFile(dist, data);
                 return resolve();
             }
